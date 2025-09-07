cc650f5b99fb91cf16f283cd421f215cff19f236
Armand <136462994+thuillart@users.noreply.github.com>
2025-09-07 23:41:01 +0200

    chore: update @convex-dev/better-auth to version 0.8.0-alpha.8 and better-auth to version 1.3.7; add a new line in HomePage component for token retrieval

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -7,6 +7,7 @@ import { api } from "../../convex/_generated/api";
 
 export default async function HomePage() {
 	const token = await getToken(createAuth);
+
 	const user = await fetchQuery(api.auth.getUser, {}, { token });
 
 	if (!user) {
